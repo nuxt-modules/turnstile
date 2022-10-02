51422d99cc7aeb25375ad40fe56155e2d3ad759c
Daniel Roe <daniel@roe.dev>
2022-10-02 19:13:31 +0100

    fix: encode body of verify request

--- src/runtime/nitro/utils/verify.ts
+++ src/runtime/nitro/utils/verify.ts
@@ -8,7 +8,7 @@ const endpoint = 'https://challenges.cloudflare.com/turnstile/v0/siteverify'
 export const verifyTurnstileToken = async (token: string): Promise<TurnstileValidationResponse> => {
   return await $fetch(endpoint, {
     method: 'POST',
-    body: `secret=${secretKey}&response=${token}`,
+    body: `secret=${encodeURIComponent(secretKey)}&response=${encodeURIComponent(token)}`,
     headers: {
       'content-type': 'application/x-www-form-urlencoded',
     },
