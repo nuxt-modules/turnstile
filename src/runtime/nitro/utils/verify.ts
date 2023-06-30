6a60b99f007683002555b70292537122d5b604de
Yizack Rangel <yizackr@gmail.com>
2023-06-30 12:04:03 +0100

    feat: add optional `event` for serverless (#257)

--- src/runtime/nitro/utils/verify.ts
+++ src/runtime/nitro/utils/verify.ts
@@ -1,12 +1,13 @@
+import type { H3Event } from 'h3'
 import type { TurnstileValidationResponse } from '../../types'
 
 // @ts-expect-error nitro aliases aren't registered
 import { useRuntimeConfig } from '#internal/nitro'
 
-const secretKey = useRuntimeConfig().turnstile.secretKey
 const endpoint = 'https://challenges.cloudflare.com/turnstile/v0/siteverify'
 
-export const verifyTurnstileToken = async (token: string): Promise<TurnstileValidationResponse> => {
+export const verifyTurnstileToken = async (token: string, event?: H3Event): Promise<TurnstileValidationResponse> => {
+  const secretKey = useRuntimeConfig(event).turnstile.secretKey
   return await $fetch(endpoint, {
     method: 'POST',
     body: `secret=${encodeURIComponent(secretKey)}&response=${encodeURIComponent(token)}`,
