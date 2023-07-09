60035431f3e8f07970d61648554447ab8c945fff
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-07-10 01:22:11 +0900

    chore(eslint): format

--- src/runtime/nitro/utils/verify.ts
+++ src/runtime/nitro/utils/verify.ts
@@ -6,7 +6,10 @@ import { useRuntimeConfig } from '#internal/nitro'
 
 const endpoint = 'https://challenges.cloudflare.com/turnstile/v0/siteverify'
 
-export const verifyTurnstileToken = async (token: string, event?: H3Event): Promise<TurnstileValidationResponse> => {
+export const verifyTurnstileToken = async (
+  token: string,
+  event?: H3Event
+): Promise<TurnstileValidationResponse> => {
   const secretKey = useRuntimeConfig(event).turnstile.secretKey
   return await $fetch(endpoint, {
     method: 'POST',
