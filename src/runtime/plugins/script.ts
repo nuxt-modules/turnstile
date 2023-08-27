9ce9e2d034f8871c9973f6aeb1f32babdead12bd
Daniel Roe <daniel@roe.dev>
2023-08-27 16:08:48 +0100

    style: lint

--- src/runtime/plugins/script.ts
+++ src/runtime/plugins/script.ts
@@ -1,6 +1,6 @@
 import type { TurnstileRenderOptions } from '../types'
 import { defineNuxtPlugin, useRuntimeConfig, ref, isVue2, watch } from '#imports'
-import { useHead } from "#app"
+import { useHead } from '#app'
 
 const configure = [
   'window.loadTurnstile = new Promise(resolve => {',
