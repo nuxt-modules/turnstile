11fba329168b3f300b61f4b7eafaab00adfa41be
Yizack Rangel <yizackr@gmail.com>
2025-03-09 06:07:52 -0500

    chore: revert module in config

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-09-04',
-  modules: ['../src/module'],
+  modules: ['@nuxtjs/turnstile'],
   runtimeConfig: {
     turnstile: {
       secretKey: '1x0000000000000000000000000000000AA',
